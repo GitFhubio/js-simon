916c8d729a9b26ec11f3a559a3b8ee257edae4d4
Fabio Petrone <fa610movic90@gmail.com>
2021-01-15 13:38:25 +0100

    Script base

---
+++ js/script.js
@@ -0,0 +1,69 @@
+
+function isValid(num) {
+  if(isNaN(num)) {
+    return false;
+  }
+  if(num < 1) {
+    return false;
+  }
+  if(num >100) {
+    return false;
+  }
+  return true;
+}
+
+$(document).ready(function(){
+var arr=[];
+var RandomNumber;
+while (arr.length<5) {
+RandomNumber=Math.floor(Math.random()*100+1);
+if (arr.indexOf(RandomNumber)==-1){
+
+  arr.push(RandomNumber);
+}
+}
+console.log(arr);
+alert('Questi sono i numeri che devi memorizzare: '+arr);
+
+
+setTimeout(function(){
+
+var arrScelte=[];
+var SceltaUtente;
+var c=0;
+while (arrScelte.length<5 ) {
+
+SceltaUtente=parseInt(prompt('Inserisci il '+(c+1)+'°'+' numero'));
+if(arrScelte.indexOf(SceltaUtente)!=-1)
+{alert('Hai già inserito questo numero');
+}
+else if(!isValid(SceltaUtente))
+{alert('Erano numeri da 1 a 100,non te lo conto manco errore');
+}
+else{
+  arrScelte.push(SceltaUtente);
+ c++;
+}
+}
+var scelteGiuste=[];
+for (var i = 0; i < arrScelte.length; i++) {
+if(arr.indexOf(arrScelte[i])!=-1)
+{
+  scelteGiuste.push(arrScelte[i]);
+}
+}
+switch(scelteGiuste.length){
+case 0:
+alert('Non hai indovinato manco un numero,hai problemi di memoria grossi');
+break;
+case 1:
+alert('Hai indovinato solo il numero '+[scelteGiuste]);
+break;
+default:
+alert('Hai indovinato '+scelteGiuste.length+' numeri: '+[scelteGiuste]);
+}
+  ; },
+ 30000);
+
+}
+)
